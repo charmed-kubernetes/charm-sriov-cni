539465c91424252733ee429dec087566be97b7a4
Adam Dyess <adam.dyess@canonical.com>
2023-01-06 16:31:14 -0600

    include the control-plane toleration in the daemonsets

--- tests/unit/test_manifests.py
+++ tests/unit/test_manifests.py
@@ -8,7 +8,40 @@ from typing import List, Tuple
 
 import pytest
 
-from manifests import SetCNIPath
+from manifests import SetCNIPath, UpdateDaemonSetTolerations
+
+
+def test_update_tolerations(harness):
+    harness.disable_hooks()
+    harness.begin_with_initial_hooks()
+    patch = UpdateDaemonSetTolerations(harness.charm.manifests)
+    toleration = mock.MagicMock()
+    toleration.key = "node-role.kubernetes.io/important"
+    toleration.operator = "Exists"
+    toleration.effect = "NoSchedule"
+
+    obj = mock.MagicMock()
+    obj.kind = "DaemonSet"
+    obj.metadata.name = "kube-sriov-cni-ds-amd64"
+    obj.spec.template.spec.tolerations = [toleration]
+    patch(obj)
+    assert len(obj.spec.template.spec.tolerations) == 2
+    assert any(
+        t.key.endswith("control-plane") for t in obj.spec.template.spec.tolerations
+    )
+    assert any(t.key.endswith("important") for t in obj.spec.template.spec.tolerations)
+
+
+def test_update_tolerations_only_changes_recognized_daemonset(
+    harness,
+):
+    harness.disable_hooks()
+    harness.begin_with_initial_hooks()
+    patch = UpdateDaemonSetTolerations(harness.charm.manifests)
+    obj = mock.Mock(kind="DaemonSet", **{"metadata.name": "not-sriovdp"})
+    patch(obj)
+    # Mock object would raise a TypeError
+    # if any attempt to access the spec is attempted
 
 
 @pytest.mark.parametrize(
